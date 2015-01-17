6c02f1388e8f89938782b41d513fdd4aa46addcb
Jesse Robinson <j3robinson1@gmail.com>
2015-01-16 23:48:07 -0500

    added some things

--- blog.rb
+++ blog.rb
@@ -5,7 +5,12 @@ DataMapper.setup(
 	:default,
 	'mysql://root@localhost/blog'
 	)
-
+class User
+	include DataMapper::Resource
+	property :id, Serial
+	property :username, String
+	property :password, String
+end
 class Blog
 		include DataMapper::Resource
 		property :id, Serial
@@ -17,9 +22,39 @@ DataMapper.finalize.auto_upgrade!
 
 get '/' do
 	@blogs = Blog.all
+	@users = User.all
 	erb :index
 end
-
+get '/login' do
+	erb :login
+end
+post '/create-user' do
+	p params
+	@user = User.new
+	@user.username = params[:username]
+	@user.password = params[:password]
+	@user.save
+	redirect to '/'
+end
+get '/user/:id' do
+	@user = User.get params[:id]
+	erb :show_user
+end
+delete '/delete-user/:id' do
+	@user = User.get params[:id]
+	@user.destroy
+	redirect to '/'
+end
+get'/edit-user/:id' do
+	p params
+	@user = User.get params[:id]
+	erb :edit_user
+end
+patch '/save-user/:id' do
+	@user = User.get params[:id]
+	@user.update password:params[:password]
+	redirect to '/'
+end
 get '/new_blog' do
 	erb :create_blog
 end
